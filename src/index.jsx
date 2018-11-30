715965e1fdfd85f979f1be80f6b9bb11745a41b7
= <josericardo29@hotmail.es>
2018-11-30 11:33:14 -0400

    Making pulll

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import { render } from 'react-dom'
+
+class Test extends Component {
+    
+    render () {
+        return (
+            <h1>Reacttr</h1>
+        )
+    }
+}
+
+render(<Test />, document.getElementById('root'))
+
